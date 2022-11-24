3308cf1306e00a54903b22f21bf34550cbd3a7a8
Shirin Ermis <ermis@atmlxint1.atm.ox.ac.uk>
2022-11-24 15:55:18 +0000

    added data.py file and in init

---
+++ stormeunice/data.py
@@ -0,0 +1,27 @@
+'''
+Functions to import data
+'''
+
+class Data():
+    """
+    Class to import data files
+    """
+
+    def __init__(self):
+        self.image = None
+
+    def load_data(dir, filename):
+        """
+        Function loads the data in directory dir and file fielname
+
+        Input
+        -----
+        dir: str, path to directory
+        filename: str, filename of data
+
+        Output
+        ------
+        data: 
+        """
+
+        
